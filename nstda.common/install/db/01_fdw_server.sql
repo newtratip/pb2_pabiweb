169749c850de745ba2106263fd0cb2752a01d55b
Thongchai Jiansampimon <thongchai@bgear.co.th>
2016-11-04 12:04:12 +0700

    V.1.0.85.6

--- nstda.common/install/db/01_fdw_server.sql
+++ nstda.common/install/db/01_fdw_server.sql
@@ -1,2 +1,7 @@
-ALTER server foreign_server  OPTIONS (SET host '10.226.202.133', SET port '5432', SET dbname 'PABI2_v4');
-ALTER USER MAPPING FOR alfresco SERVER foreign_server OPTIONS (SET user 'openerp', SET password 'VZbv29Nl');
+CREATE EXTENSION postgres_fdw;
+CREATE SERVER foreign_server
+        FOREIGN DATA WRAPPER postgres_fdw
+        OPTIONS (host '10.226.202.133', port '5432', dbname 'PABI2_v7');
+CREATE USER MAPPING FOR alfresco
+        SERVER foreign_server
+        OPTIONS (user 'openerp', password 'VZbv29Nl');
